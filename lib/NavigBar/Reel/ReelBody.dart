d719f8fc306b8822250c7cba7d70152424cc2697
heyprincesingh <princesingh3632@gmail.com>
2021-12-21 02:32:48 +0530

    Instagram Clone

--- lib/NavigBar/Reel/ReelBody.dart
+++ lib/NavigBar/Reel/ReelBody.dart
@@ -12,6 +12,7 @@ class reelbody extends StatefulWidget {
 }
 
 class _reelbodyState extends State<reelbody> {
+  bool descriptionclick = false;
   bool like = false;
   bool follow = false;
   @override
@@ -35,7 +36,6 @@ class _reelbodyState extends State<reelbody> {
             loop: false,
             itemBuilder: (BuildContext context,int index){
               return Stack(
-                //  fit: StackFit.expand,
                 children: [
                   Container(
                     height: device.height * 0.882,
@@ -51,53 +51,78 @@ class _reelbodyState extends State<reelbody> {
                       left: 18,
                       child: Text("Reels",style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),)),
                   Positioned(
-                      bottom: 85,
+                      bottom: 48,
                       left: 18,
-                      child: CircleAvatar(
-                        radius: 16.0,
-                        backgroundImage:
-                        NetworkImage(myreeldata[index]["dpurl"]),
-                        backgroundColor: Colors.transparent,
-                      )),
-                  Positioned(
-                      bottom: 85,
-                      left: 65,
-                      child: Row(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(myreeldata[index]["user"],style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CircleAvatar(
+                                radius: 16.0,
+                                backgroundImage:
+                                NetworkImage(myreeldata[index]["dpurl"]),
+                                backgroundColor: Colors.transparent,
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Text(myreeldata[index]["user"],style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              GestureDetector(
+                                onTap: (){ setState(() {
+                                  follow = !follow;
+                                }); },
+                                child: follow ? Container(
+                                  child: Center(child: Text("Following",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15),)),
+                                  width: 90,
+                                  height: 28,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.white),
+                                    borderRadius: BorderRadius.all(Radius.circular(6)),
+                                  ),
+                                ) : Container(
+                                  child: Center(child: Text("Follow",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15),)),
+                                  width: 70,
+                                  height: 28,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.white),
+                                    borderRadius: BorderRadius.all(Radius.circular(6)),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                           SizedBox(
-                            width: 15,
+                            height: 10,
                           ),
                           GestureDetector(
-                            onTap: (){ setState(() {
-                              follow = !follow;
-                            }); },
-                            child: follow ? Container(
-                              child: Center(child: Text("Following",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15),)),
-                              width: 90,
-                              height: 28,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.white),
-                                borderRadius: BorderRadius.all(Radius.circular(6)),
-                              ),
+                            onTap: (){
+                              setState(() {
+                                descriptionclick = !descriptionclick;
+                              });
+                            },
+                            child: descriptionclick ?
+                            Container(
+                                width: device.width * 0.7,
+                                child: Text(myreeldata[index]["description"],
+                                  maxLines: 40,
+                                  style: TextStyle(fontSize: 16),)
                             ) : Container(
-                              child: Center(child: Text("Follow",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15),)),
-                              width: 70,
-                              height: 28,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.white),
-                                borderRadius: BorderRadius.all(Radius.circular(6)),
-                              ),
+                                width: 160,
+                                child: Text(myreeldata[index]["description"],
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(fontSize: 16),)
                             ),
                           )
-                        ],
-                      )),
+                        ]
+                      )
+                  ),
                   Positioned(
-                      bottom: 50,
-                      left: 18,
-                      child: Text(myreeldata[index]["description"],style: TextStyle(fontSize: 16),)),
-                  Positioned(
-                      bottom: 20,
+                      bottom: 18,
                       left: 18,
                       child: Row(
                         children: [
